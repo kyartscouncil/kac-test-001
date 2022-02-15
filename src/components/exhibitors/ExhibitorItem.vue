6e4d25d94cbdfa245be939f7209f219cebcabc6e
kac-ershe <87831734+kac-ershe@users.noreply.github.com>
2022-02-15 15:20:45 -0500

    Created Base components, etc

---
+++ src/components/exhibitors/ExhibitorItem.vue
@@ -0,0 +1,63 @@
+<template>
+	<li>
+		<h4 class="float-right">Booth #: {{ boothNumber }}</h4>
+		<h2>{{ businessName }}</h2>
+		<h3>{{ fullName }}</h3>
+		<div>
+			<base-badge v-for="medium in media" :key="medium" :type="medium" :title="medium"></base-badge>
+		</div>
+		<div class="actions">
+			<base-button mode="outline" link :to="exhibitorDetailsLink">View Details</base-button>
+		</div>
+	</li>
+</template>
+
+<script>
+	export default {
+		props: ['id', 'businessName', 'firstName', 'lastName', 'boothNumber', 'media'],
+		computed: {
+			fullName() {
+				return this.firstName + ' ' + this.lastName;
+			},
+			exhibitorDetailsLink() {
+				return this.$route.path + '/' + this.id;
+			},
+		},
+	};
+</script>
+
+<style scoped>
+	li {
+		margin: 1rem 0;
+		border: 1px solid #424242;
+		border-radius: 12px;
+		padding: 1rem;
+	}
+
+	h2 {
+		font-size: 2rem;
+	}
+
+	h3 {
+		font-size: 1.5rem;
+	}
+
+	h2,
+	h3,
+	h4 {
+		margin: 0.5rem 0;
+	}
+
+	div {
+		margin: 0.5rem 0;
+	}
+
+	.actions {
+		display: flex;
+		justify-content: flex-end;
+	}
+
+	.float-right {
+		float: right;
+	}
+</style>
--- src/pages/exhibitors/ExhibitorsList.vue
+++ src/pages/exhibitors/ExhibitorsList.vue
@@ -1,18 +1,36 @@
 <template>
-	<section>Filter</section>
 	<section>
-		<div class="controls">
-			<button>Shuffle</button>
-		</div>
-		<ul v-if="hasExhibitors">
-			<li v-for="exhibitor in filteredExhibitors" :key="exhibitor.id">{{ exhibitor.firstName }}</li>
-		</ul>
-		<h3 v-else>No exhibitors found.</h3>
+		<h2>Filter</h2>
+	</section>
+	<section>
+		<base-card>
+			<div class="controls">
+				<base-button mode="outline">Shuffle</base-button>
+			</div>
+			<ul v-if="hasExhibitors">
+				<exhibitor-item
+					v-for="exhibitor in filteredExhibitors"
+					:key="exhibitor.id"
+					:id="exhibitor.id"
+					:business-name="exhibitor.businessName"
+					:first-name="exhibitor.firstName"
+					:last-name="exhibitor.lastName"
+					:booth-number="exhibitor.boothNumber"
+					:media="exhibitor.media"
+				></exhibitor-item>
+			</ul>
+			<h3 v-else>No exhibitors found.</h3>
+		</base-card>
 	</section>
 </template>
 
 <script>
+	import ExhibitorItem from '../../components/exhibitors/ExhibitorItem.vue';
+
 	export default {
+		components: {
+			ExhibitorItem,
+		},
 		computed: {
 			filteredExhibitors() {
 				return this.$store.getters['exhibitors/exhibitors'];
@@ -23,3 +41,16 @@
 		},
 	};
 </script>
+
+<style scoped>
+	ul {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	.controls {
+		display: flex;
+		justify-content: space-between;
+	}
+</style>
