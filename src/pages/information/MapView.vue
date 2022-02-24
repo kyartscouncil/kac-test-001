4c2d67818c92683f8d566fff1148c1bb4320f1a1
kyartscouncil <97762840+kyartscouncil@users.noreply.github.com>
2022-02-24 16:27:24 -0500

    map changes

--- src/pages/information/MapView.vue
+++ src/pages/information/MapView.vue
@@ -1,10 +1,12 @@
 <template>
-	<img class="floor-plan" src="../../assets/m22-floor-plan-feb15.jpg" alt="market floor plan alltech arena" />
+	<a href="../../assets/m22-floor-plan-feb15.jpg"><img class="floor-plan" src="../../assets/m22-floor-plan-feb15.jpg" alt="market floor plan alltech arena" /></a>
 </template>
 
 <style scoped>
 	.floor-plan {
-		width: 100%;
-      transform: rotate(180deg);
+      display: block;
+		transform: rotate(270deg);
+		width: 70%;
+      margin: 0 auto;
 	}
 </style>
